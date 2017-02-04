9d9edb6e12bd297a298aacf438a44b6c2f76e4ed
daniel8450 <dlabo14@lsu.edu>
2017-02-04 07:24:12 -0600

    major game changes

---
+++ Assets/LEFTflipperInput.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LEFTflipperInput : MonoBehaviour
+{
+
+    public float force;
+    Rigidbody2D flipper;
+
+
+    void Start()
+    {
+
+        flipper = GetComponent<Rigidbody2D>();
+
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            flipper.AddTorque(force);
+        }
+
+    }
+}
---
+++ Assets/RIGHTflipperInput.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RIGHTflipperInput : MonoBehaviour
+{
+
+    public float force;
+    Rigidbody2D flipper;
+
+
+    void Start()
+    {
+
+        flipper = GetComponent<Rigidbody2D>();
+
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            flipper.AddTorque(force);
+        }
+
+    }
+}
